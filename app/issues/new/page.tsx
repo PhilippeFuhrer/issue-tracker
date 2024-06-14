50773b95791170db47cd9f612889af460f014760
Philippe.Fuhrer <Philippe.Fuhrer@hotmail.com>
2024-06-14 09:37:54 +0200

    Add MDE Editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,22 @@
-import { TextField } from '@radix-ui/themes'
-import { TextArea } from '@radix-ui/themes'
-import { Button } from '@radix-ui/themes'
+'use client';
 
-import React from 'react'
+import { TextField } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+import React from "react";
 
 const newIssuePage = () => {
   return (
-    <div className = 'max-w-xl space-y-3'>
-        <TextField.Root placeholder="Title">
-            <TextField.Slot>
-            </TextField.Slot>
-        </TextField.Root>
-        <TextArea placeholder="Description" />
-        <Button>Submit new Issue</Button>
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title">
+        <TextField.Slot></TextField.Slot>
+      </TextField.Root>
+      <SimpleMDE />
+      <Button>Submit new Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default newIssuePage
+export default newIssuePage;
